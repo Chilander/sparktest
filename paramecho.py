1e163ad799463db9e67cd34ac4af2e418252bc8c
Wandi Zhang <wandi.zhang@bethsoft.com>
2017-06-20 10:40:14 -0400

    Added environment variable testing.

--- paramecho.py
+++ paramecho.py
@@ -3,3 +3,6 @@ import sys
 
 print 'Number of arguments:', len(sys.argv), 'arguments.'
 print 'Argument List:', str(sys.argv)
+
+print os.environ['SOME_ENV_VARS']
+print os.environ['SOME_OTHER_ENV_VARS']
