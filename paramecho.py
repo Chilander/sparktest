64c6910279de6dce09be6c37c3e70a1717f55e8e
Wandi Zhang <wandi.zhang@bethsoft.com>
2017-06-20 11:24:51 -0400

    Fixing build error.

--- paramecho.py
+++ paramecho.py
@@ -5,5 +5,5 @@ import sys
 print 'Number of arguments:', len(sys.argv), 'arguments.'
 print 'Argument List:', str(sys.argv)
 
-print os.environ['SOME_ENV_VARS']
-print os.environ['SOME_OTHER_ENV_VARS']
+print 'SOME_ENV_VARS=', os.environ['SOME_ENV_VARS']
+print 'SOME_OTHER_ENV_VARS=', os.environ['SOME_OTHER_ENV_VARS']
